ed910022f856666e08e9c3e2887fb78e04eca6ee
Александр Мосин <alexmosin@inbox.ru>
2024-07-24 13:05:14 +0400

    Задание 3 - приветственное сообщение

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿string greeting = "Доброго времени суток";
+string name;
+
+Console.Write("Введите ваше имя: ");
+name = Console.ReadLine();
+
+while (string.IsNullOrEmpty(name))
+{
+    Console.WriteLine("Имя не может быть пустым.");
+    Console.Write("Введите ваше имя: ");
+    name = Console.ReadLine();
+}
+
+Console.WriteLine($"{greeting}, {name}!");
