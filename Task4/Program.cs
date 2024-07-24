a84c099347c8738a911fc09496fd42e697f9ab04
Александр Мосин <alexmosin@inbox.ru>
2024-07-24 13:11:28 +0400

    Четыёртое задание - объединение двух строк в одно предложение

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿string firstPhrase;
+string secondPhrase;
+
+Console.Write("Введите первую фразу: ");
+firstPhrase = Console.ReadLine();
+
+Console.Write("Введите вторую фразу: ");
+secondPhrase = Console.ReadLine();
+
+Console.WriteLine($"{firstPhrase} {secondPhrase}");
+
+Console.ReadKey();
