9d0d332a6d54b5c58340e7b9059fbc9224315b90
Александр Мосин <alexmosin@inbox.ru>
2024-07-29 19:49:28 +0400

    9 - создание массива целых чисел

---
+++ Practice2.Task9/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] getIntArray(int n)
+    {
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите элемент под номером {0}: ", i);
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void printArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+var arr = getIntArray(5);
+printArray(arr);
