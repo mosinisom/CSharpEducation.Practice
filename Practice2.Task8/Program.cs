a1ab5dd790aaeba53280cc6ef6d37eea9db9dc65
Александр Мосин <alexmosin@inbox.ru>
2024-07-29 19:49:10 +0400

    8 - увеличение на 5, если два равных

---
+++ Practice2.Task8/Program.cs
@@ -0,0 +1,33 @@
+﻿double[] arr = new double[3];
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"Ведите число {i + 1}: ");
+    arr[i] = double.Parse(Console.ReadLine());
+}
+
+bool isAllUnique = true;
+for(int i = 0; i < arr.Length; i++)
+{
+    for(int j = i + 1; j < arr.Length; j++)
+    {
+        if(arr[i] == arr[j])
+        {
+            isAllUnique = false;
+            break;
+        }
+    }
+}
+
+if(isAllUnique)
+{
+    Console.WriteLine("Равных нет");
+}
+else
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] += 5;
+    }
+}
+
+Console.WriteLine(arr[0] + " " + arr[1] + " " + arr[2]);
