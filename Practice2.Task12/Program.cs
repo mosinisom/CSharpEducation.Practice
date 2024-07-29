13832db21e049365152052eda450b8af2d2cd6b0
Александр Мосин <alexmosin@inbox.ru>
2024-07-29 19:50:55 +0400

    12 - массив строк, заполнение через консоль

---
+++ Practice2.Task12/Program.cs
@@ -0,0 +1,16 @@
+﻿int n;
+
+Console.WriteLine("Введите количество элементов массива: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+string[] strings = new string[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите строку под номером {0}: ", i);
+    strings[i] = Console.ReadLine();
+}
+
+for (int i = 0; i < strings.Length; i++)
+{
+    Console.Write(strings[i] + " ");
+}
