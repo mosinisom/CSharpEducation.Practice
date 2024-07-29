09a95ebb10da58f2b595894df66c67f6657f6fbf
Александр Мосин <alexmosin@inbox.ru>
2024-07-29 19:50:30 +0400

    11 - массив, размер и число через аргумент командной строки

---
+++ Practice2.Task11/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int n;
+        int number;
+        void createAndPrintArray()
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Введите количество элементов массива: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите число: ");
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            else
+            {
+                n = Convert.ToInt32(args[0]);
+                number = Convert.ToInt32(args[1]);
+            }
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = number;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+        createAndPrintArray();
+
+    }
+}
