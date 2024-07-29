a5d7786dfa1cab691450a1bf260dbd130119dc5f
Александр Мосин <alexmosin@inbox.ru>
2024-07-29 19:47:58 +0400

    5 - сравнение двух целых чисел

---
+++ Practice2.Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿int a;
+int b;
+
+Console.WriteLine("Сравнение двух чисел");
+Console.Write("Введите a: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите b: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+    {
+    Console.WriteLine($"a больше b на {a - b}");
+}
+else if(a < b)
+{
+    Console.WriteLine($"b больше a на {b - a}");
+}
+else
+{
+    Console.WriteLine("a и b равны");
+}
