c6d8431317eb7e2979208124d2249307bfd586da
Александр Мосин <alexmosin@inbox.ru>
2024-07-29 19:48:42 +0400

    7 - перевод регистров

---
+++ Practice2.Task7/Program.cs
@@ -0,0 +1,11 @@
+﻿string toUpper = "hello world";
+Console.WriteLine(toUpper + " -> " + toUpper.ToUpper());
+
+string toLower = "HELLO WORLD";
+Console.WriteLine(toLower + " -> " + toLower.ToLower());
+
+string firstLetterUpper = "hello world";
+Console.WriteLine(firstLetterUpper + " -> " + char.ToUpper(firstLetterUpper[0]) + firstLetterUpper.Substring(1));
+
+Console.ReadKey();
+
