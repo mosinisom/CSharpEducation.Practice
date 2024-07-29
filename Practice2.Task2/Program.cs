6a92811e427be03272e33183a922f253e3719da5
Александр Мосин <alexmosin@inbox.ru>
2024-07-29 19:47:04 +0400

    2 - перевод значений

---
+++ Practice2.Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿// a
+void mToKm(double m)
+{
+    double km = m / 1000;
+    Console.WriteLine($"{m} метр(-ов) это {km} километр(-ов)");
+}
+mToKm(1345);
+
+Console.ReadKey();
+
+// b
+void kmToCm(double km)
+{
+    double cm = km * 100000;
+    Console.WriteLine($"{km} километр(-ов) это {cm} сантиметр(-ов)");
+}
+kmToCm(3.5);
+
+Console.ReadKey();
+
+// c
+void msToKmh(double ms)
+{
+    double kmh = ms * 3.6;
+    Console.WriteLine($"{ms} метр(-ов) в секунду это {kmh} километр(-ов) в час");
+}
+msToKmh(15);
+
+Console.ReadKey();
+
+// d
+void cToF(double c)
+{
+    double f = c * 9 / 5 + 32;
+    Console.WriteLine($"{c} градус(-ов) Цельсия это {f} градус(-ов) Фаренгейта");
+}
+cToF(36.6);
+
+Console.ReadKey();
