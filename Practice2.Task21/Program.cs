a0b439e8693483f8b82a8e6aef66d6925d6b780d
Александр Мосин <alexmosin@inbox.ru>
2024-07-31 14:13:58 +0400

    21 - угадай число от 1 до 100

---
+++ Practice2.Task21/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        GuessTheNumber();
+    }
+
+    static void GuessTheNumber()
+    {
+        Random rand = new Random();
+        int number = rand.Next(1, 100);
+        int attempts = 0;
+        Console.WriteLine("Угадай число от 1 до 100.");
+        while (true)
+        {
+            Console.Write("Введи число: ");
+            int guess = int.Parse(Console.ReadLine());
+            attempts++;
+            if (guess == number)
+            {
+                Console.WriteLine($"Поздравляю! Ты угадал число за {attempts} попыток.");
+                break;
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Число больше.");
+            }
+            else
+            {
+                Console.WriteLine("Число меньше.");
+            }
+        }
+    }
+
+}
