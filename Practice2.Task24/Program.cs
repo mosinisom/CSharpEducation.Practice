b5ac4a01097fff94f0685c2f14653edcf5fc7b79
Александр Мосин <alexmosin@inbox.ru>
2024-07-31 14:15:16 +0400

    24 - enum для дней недели, вывод всех значений

---
+++ Practice2.Task24/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        PrintDaysOfWeek();
+    }
+
+    enum DaysOfWeek
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    static void PrintDaysOfWeek()
+    {
+        foreach (DaysOfWeek day in Enum.GetValues(typeof(DaysOfWeek)))
+        {
+            Console.WriteLine(day);
+        }
+    }
+}
