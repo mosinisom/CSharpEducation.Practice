a8b603ff2b5c67499bd6411b1bae8ee6f358735e
Александр Мосин <alexmosin@inbox.ru>
2024-07-31 14:11:15 +0400

    16 - менять знак числа по ссылке (ref)

---
+++ Practice2.Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 5;
+        Console.WriteLine("Число до изменения знака: " + number);
+        ChangeSign(ref number);
+        Console.WriteLine("Число после изменения знака: " + number);
+
+    }
+
+    static void ChangeSign(ref int number)
+    {
+        number *= -1;
+    }
+   
+
+}
