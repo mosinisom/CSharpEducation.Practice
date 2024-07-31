149a422ba6d21d7c14d29cf17ed3eb0e57627998
Александр Мосин <alexmosin@inbox.ru>
2024-07-31 14:10:39 +0400

    14 - массив размером n и заполнение

---
+++ Practice2.Task14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 5;
+
+        int[] array = CreateArray(n);
+        FillArray(array);
+        PrintArray(array);
+    }
+    static int[] CreateArray(int n)
+    {
+        return new int[n];
+    }
+
+    static void FillArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i;
+        }
+    }
+
+    static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
