d0d717febb4a65ab0f140222d416d6f225c1bb45
Александр Мосин <alexmosin@inbox.ru>
2024-07-31 14:12:22 +0400

    18 - метод, который убирает отрицательные числа из массива

---
+++ Practice2.Task18/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = {1, -2, 3, -4, 5, -6, 7, -8, 9, -10};
+        int countDeleted;
+        int[] result = DeleteNegative(nums, out countDeleted);
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine(result[i]);
+        }
+
+        Console.WriteLine("Удалено отрицательных чисел: " + countDeleted);
+
+    }
+
+    static int[] DeleteNegative(int [] nums, out int countDeleted)
+    {
+       int count = 0;
+       for (int i = 0; i < nums.Length; i++)
+       {
+           if (nums[i] < 0)
+           {
+               count++;
+           }
+       }
+
+       int[] result = new int[nums.Length - count];
+       int index = 0;
+       for (int i = 0; i < nums.Length; i++)
+       {
+           if (nums[i] >= 0)
+           {
+               result[index] = nums[i];
+               index++;
+           }
+       }
+
+       countDeleted = count;
+       return result;
+    }
+}
