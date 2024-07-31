da9edae7c403bc5b7c088ebd2beb745cbec0684a
Александр Мосин <alexmosin@inbox.ru>
2024-07-31 14:13:06 +0400

    19 - массив, заполнить рандомно, вывести разницу между макс и мин элементом

---
+++ Practice2.Task19/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = new int[17];
+        FillArray(nums);
+        Console.WriteLine(GetMinMaxDifference(nums));
+    }
+
+    static void FillArray(int[] nums)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = rand.Next(1, 100);
+        }
+    }
+
+    static int GetMinMaxDifference(int[] nums)
+    {
+        int min = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] < min)
+            {
+                min = nums[i];
+            }
+            if (nums[i] > max)
+            {
+                max = nums[i];
+            }
+        }
+        return max - min;
+    }
+}
