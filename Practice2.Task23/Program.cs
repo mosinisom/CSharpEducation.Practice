bb2ba2c713644413cadba8b0ee3afe9d3a1dc022
Александр Мосин <alexmosin@inbox.ru>
2024-07-31 14:14:57 +0400

    23 - вычисление високосного года

---
+++ Practice2.Task23/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int year = 2024;
+        bool isLeap = IsLeapYear(year);
+        Console.WriteLine($"Является ли {year} високосным годом? : {(isLeap ? "Да" : "Нет")}");
+    }
+
+    static bool IsLeapYear(int year)
+    {
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+}
