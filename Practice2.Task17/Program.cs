65fb832cdfebbd3b8d4f543eb1901011329af064
Александр Мосин <alexmosin@inbox.ru>
2024-07-31 14:11:38 +0400

    17 - менять значения двух чисел местами

---
+++ Practice2.Task17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int firstNumber = 1;
+        int secondNumber = 2;
+
+        Console.WriteLine("Первое число: " + firstNumber);
+        Console.WriteLine("Второе число: " + secondNumber);
+
+        (firstNumber, secondNumber) = Swap(firstNumber, secondNumber);
+
+        Console.WriteLine("Первое число после смены значений: " + firstNumber);
+        Console.WriteLine("Второе число после смены значений: " + secondNumber);
+
+
+    }
+
+    static (int, int) Swap(int a, int b)
+    {
+        return (b, a);
+    }
+}
