ab4c7ac8ae8491299d31f88db3bedcea1474664c
Александр Мосин <alexmosin@inbox.ru>
2024-07-31 14:09:41 +0400

    13

---
+++ Practice2.Task13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 5;
+        int cols = 4; 
+
+        int[,] array = CreateArray(rows, cols);
+        FillArrayWithRandomNumbers(array);
+        PrintArray(array);
+    }
+
+    static int[,] CreateArray(int rows, int cols)
+    {
+        return new int[rows, cols];
+    }
+
+    static void FillArrayWithRandomNumbers(int[,] array)
+    {
+        Random random = new Random();
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = random.Next(1, 100);
+            }
+        }
+    }
+
+    static void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
