d1cc030818c6a74d5bc7229be18125289bb24850
Александр Мосин <alexmosin@inbox.ru>
2024-07-31 14:10:54 +0400

    15 - инверсия массива

---
+++ Practice2.Task15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] firstArray = { 1, 2};
+        int[] reversedArray = ReverseArray(firstArray);
+
+        PrintArray(firstArray);
+        Console.WriteLine();    
+        PrintArray(reversedArray);
+
+
+    }
+
+    static int[] ReverseArray(int[] array)
+    {
+        int[] reversedArray = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
+        {
+            reversedArray[i] = array[array.Length - 1 - i];
+        }
+        return reversedArray;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
