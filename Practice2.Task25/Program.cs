b231e8839254d30c0fc286d0a5ad65654acaaf7b
Александр Мосин <alexmosin@inbox.ru>
2024-07-31 14:15:47 +0400

    25 - enum дней недели, ввод номера дня от пользователя, вывод названия дня недели

---
+++ Practice2.Task25/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// наконец-то это последнее задание С:
+
+class Program
+{
+    static void Main()
+    {
+        int day;
+        Console.Write("Введите номер дня недели: ");
+        if (int.TryParse(Console.ReadLine(), out day))
+        {
+            PrintDayOfWeek(day);
+        }
+        else
+        {
+            Console.WriteLine("Такого дня недели не существует");
+        }
+    }
+
+    enum DaysOfWeek
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    static void PrintDayOfWeek(int day)
+    {
+        if (Enum.IsDefined(typeof(DaysOfWeek), day))
+        {
+            Console.WriteLine((DaysOfWeek)day);
+        }
+        else
+        {
+            Console.WriteLine("Такого дня недели не существует");
+        }
+    }
+}
