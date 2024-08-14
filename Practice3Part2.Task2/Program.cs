9788fd9578a27d4ea0ab3f3d1dd3dd7240376c54
Александр Мосин <alexmosin@inbox.ru>
2024-08-14 17:19:54 +0400

    2 - использование MathHelper

---
+++ Practice3Part2.Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Practice3Part2.Task1;
+
+namespace Practice3Part2.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mh = new MathHelper();
+
+            Console.WriteLine(mh.Add(1, 2));
+            Console.WriteLine(mh.Subtract(1, 2));
+            Console.WriteLine(mh.Multiply(1, 2));
+            Console.WriteLine(mh.Divide(1, 2));
+        }
+    }
+}
