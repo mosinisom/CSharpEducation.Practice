d05868f5f6a5e3c8961fe16ae7f6726d3c9da99f
Александр Мосин <alexmosin@inbox.ru>
2024-08-14 16:48:54 +0400

    2 - класс "студент", добавлен средний бал, добавлены геттер и сеттер

---
+++ Practice3.Task2/Student.cs
@@ -0,0 +1,40 @@
+﻿namespace Practice3.Task2
+{
+    internal class Student
+    {
+        public string Name;
+        public int Age;
+
+        public double AverageMark 
+        {
+            get { return AverageMark; }
+            set
+            {
+                if (value >= 0 && value <= 5)
+                {
+                    AverageMark = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Средний балл имеет значения от 0 до 5");
+                }
+            }
+        }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
