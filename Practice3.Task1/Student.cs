1964f6c18757585760b0c04e879be476c821d195
Александр Мосин <alexmosin@inbox.ru>
2024-08-14 16:48:02 +0400

    1 - класс "студент", вывод информации в консоль

---
+++ Practice3.Task1/Student.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Practice3.Task1
+{
+    internal class Student
+    {
+        public string Name;
+        public int Age;
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
