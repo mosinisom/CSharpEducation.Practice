e2a11c7853d79e3f56f30382d8f50ad087a9c837
Александр Мосин <alexmosin@inbox.ru>
2024-08-14 16:49:08 +0400

    3 - класс "калькулятор"

---
+++ Practice3.Task3/Calc.cs
@@ -0,0 +1,25 @@
+﻿namespace Practice3.Task3
+{
+    internal static class Calc
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
