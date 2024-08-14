e4a5c952fcba84eeb5f9a00c75215fa2c6c001ff
Александр Мосин <alexmosin@inbox.ru>
2024-08-14 16:49:40 +0400

    4 - класс "книга", два конструктора

---
+++ Practice3.Task4/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice3.Task4
+{
+    internal class Book
+    {
+        public string Name { get; }
+        public string Author { get; }
+
+        public Book(string name, string author)
+        {
+            Name = name;
+            Author = author;
+        }
+
+        public Book() : this("Название книги", "Автор книги")
+        {
+        }
+    }
+}
