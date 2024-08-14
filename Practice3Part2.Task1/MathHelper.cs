ded1eb403254fc57d4b676cdc0d3fdbb5767cbc5
Александр Мосин <alexmosin@inbox.ru>
2024-08-14 17:19:36 +0400

    1 - библиотека классов MathHelper с основными мат. операциями

---
+++ Practice3Part2.Task1/MathHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace Practice3Part2.Task1
+{
+    public class MathHelper
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new System.ArgumentException("Division by zero is not allowed");
+            }
+            return a / b;
+        }
+    }
+}
