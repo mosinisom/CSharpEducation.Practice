b35f6c57ae8ebd54777f131b79f4f01c3020ee2a
Александр Мосин <alexmosin@inbox.ru>
2024-08-14 17:20:21 +0400

    3 - библиотека классов Person и её использование

---
+++ Practice3Part2.Task3.1/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace Practice3Part2.Task3._1
+{
+    public class Person
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public new string ToString()
+        {
+            return $"Человеку с именем: {this.Name}  столько лет: {this.Age} !!!";
+        }
+
+        public void PrintPerson()
+        {
+            System.Console.WriteLine(ToString());
+        }
+
+    }
+}
---
+++ Practice3Part2.Task3.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using Practice3Part2.Task3._1;
+
+namespace Practice3Part2.Task3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstPerson = new Person("Egor", 25);
+            var secondPerson = new Person("Timur", 30);
+            
+            firstPerson.PrintPerson();
+            secondPerson.PrintPerson();
+        }
+    }
+}
