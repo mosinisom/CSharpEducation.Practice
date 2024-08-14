d2b1d3340569e6b6814281eabea53057d5f8d84e
Александр Мосин <alexmosin@inbox.ru>
2024-08-14 16:51:08 +0400

    7 - перечисление "месяцы"

---
+++ Practice3.Task7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+
+    static int GetDaysInMonth(Months month)
+    {
+        switch (month)
+        {
+            case Months.January:
+            case Months.March:
+            case Months.May:
+            case Months.July:
+            case Months.August:
+            case Months.October:
+            case Months.December:
+                return 31;
+            case Months.April:
+            case Months.June:
+            case Months.September:
+            case Months.November:
+                return 30;
+            case Months.February:
+                return 28;
+            default:
+                Console.WriteLine("Нет такого месяца");
+                return 0;
+        }
+    }
+}
+
+enum Months
+{
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
