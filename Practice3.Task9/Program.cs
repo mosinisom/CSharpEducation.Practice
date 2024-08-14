de363bab850fc9fe9f6a40840e84c40189de73fc
Александр Мосин <alexmosin@inbox.ru>
2024-08-14 16:52:09 +0400

    9 - класс "студент", метод, меняющий имя студента на "Аноним"

---
+++ Practice3.Task9/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice3.Task9
+{
+
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+
+
+    static void MakeStudentAnonimous(Student student)
+    {
+        student.ChangeName("Anonimous");
+    }
+
+    }
+
+
+}
---
+++ Practice3.Task9/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice3.Task9
+{
+    internal class Student
+    {
+        public string Name { get; private set; }
+        public int Age { get; }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void ChangeName(string newName)
+        {
+            Name = newName;
+        }
+    }
+}
