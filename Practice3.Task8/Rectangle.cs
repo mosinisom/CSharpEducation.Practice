3f65bddcede38a2d606754eb950ab73791c24ef0
Александр Мосин <alexmosin@inbox.ru>
2024-08-14 16:51:29 +0400

    8 - структура "прямоугольник", вычисление площади

---
+++ Practice3.Task8/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿public struct Rectangle
+{
+    double Width;
+    double Height;
+
+    public Rectangle(double width, double height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public double CalcArea()
+    {
+        return Width * Height;
+    }
+}
