5ca4fd0841134481ac2da1693b97c51049df886b
Александр Мосин <alexmosin@inbox.ru>
2024-08-14 16:52:32 +0400

    10 - класс "Автомобиль", вывод информации о марке в консоль

---
+++ Practice3.Task10/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3.Task10
+{
+    internal class Car
+    {
+        public string Brand { get; private set; }
+
+        public Car(string brand)
+        {
+            Brand = brand;
+        }
+
+        public string ToString()
+        {
+            return Brand;
+        }
+    }
+}
---
+++ Practice3.Task10/Util.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice3.Task10
+{
+    internal class Util
+    {
+        public static void PrintCarBrand(Car car)
+        {
+            Console.WriteLine(car.ToString());
+        }
+    }
+}
