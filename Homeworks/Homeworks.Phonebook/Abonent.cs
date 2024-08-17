5b9561c696b93e83f2b44eb4d5f449d969898bf7
alex mosin <alexmosin@inbox.ru>
2024-08-17 21:01:19 +0400

    третье домашнее - Phonebook

---
+++ Homeworks/Homeworks.Phonebook/Abonent.cs
@@ -0,0 +1,11 @@
+internal class Abonent
+{
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public Abonent(string name, string phoneNumber)
+    {
+        Name = name;
+        PhoneNumber = phoneNumber;
+    }
+}
---
+++ Homeworks/Homeworks.Phonebook/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // тут мог быть пример работы с Phonebook, но в тз его нет, значит и тут его нет
+    }
+}
